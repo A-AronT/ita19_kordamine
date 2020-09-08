9a9dd61062d813e7fc0d7db54d3de25717c7ba37
Aron Truu <aron.truu@khk.ee>
2020-09-08 20:15:19 +0300

    Praegu veel ilma komentaarideta, tegin raamistiku ülesande täitmiseks lõpuni, tuleb lihtsalt veel ülesande tegeliku sisuga tegeleda.

--- laulja.py
+++ laulja.py
@@ -3,6 +3,9 @@ class Laulja:
     def __init__(self, nimi):
         self.nimi = nimi
         self.laulud = []
+        self.albumid = []
 
     def lisa_album(self, album):
         self.albumid.append(album)
+        for i in album.laulud:
+            self.laulud.append(i)
--- menüü.py
+++ menüü.py
@@ -2,8 +2,28 @@ from laul import Laul
 from album import Album
 from laulja import Laulja
 tabel = []
-fail=open("albumid.txt")
+albumid = []
+lauljad = []
+albumid_olemas = []
+lauljad_olemas = []
+fail=open("albumid.txt", encoding="UTF-8")
 for rida in fail:
-    tabel.append(rida.split("/t"))
+    tabel.append(rida.strip().split("\t"))
+fail.close()
 for i in tabel:
-    print(i)
+    if i[1] not in albumid_olemas:
+        temp = Album(i[1],i[2],i[0])
+        albumid.append(temp)
+        albumid_olemas.append(i[1])
+    if i[0] not in lauljad_olemas:
+        temp = Laulja(i[0])
+        lauljad.append(temp)
+        lauljad_olemas.append(i[0])
+    teemp = Laul(i[3], i[0])
+    for i in albumid:
+        if i.laulja == teemp.laulja:
+            i.lisa_laul(teemp)
+for i in albumid:
+    for j in lauljad:
+        if i.laulja == j.nimi:
+            j.lisa_album(i)
